114d34b9bbc1a849c5124165db26e6749a214ae4
wanjirudiana <146855192+wanjirudiana@users.noreply.github.com>
2024-10-23 16:29:30 +0300

    Time: 0 ms (100%), Space: 22 MB (29.02%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,34 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        // If the tree is empty, we can say it’s balanced...
-        if (root == NULL)  return true;
-        // Height Function will return -1, when it’s an unbalanced tree...
-		if (Height(root) == -1)  return false;
-		return true;
+    int maxdepth(TreeNode* root, bool& res) {
+        if (root == NULL)
+            return 0;
+        int l = maxdepth(root->left, res);
+        int r = maxdepth(root->right, res);
+        if(abs(l-r)>1)res=false;
+        return max(l+1,r+1);
     }
-   // Create a function to return the “height” of a current subtree using recursion...
-	int Height(TreeNode* root) {
-        // Base case...
-		if (root == NULL)  return 0;
-        // Height of left subtree...
-		int leftHeight = Height(root->left);
-        // Height of height subtree...
-		int rightHight = Height(root->right);
-        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
-		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
-        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-		return max(leftHeight, rightHight) + 1;
-    } 
+    bool isBalanced(TreeNode* root) {
+        bool res = true;
+        maxdepth(root, res);
+        return res;
+    }
 };
